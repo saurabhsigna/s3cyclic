e0a00a0134399acf3e13082d7fe65cc603cd5db8
saurabh pal <131267391+saurabhsigna@users.noreply.github.com>
2023-05-19 16:57:08 +0000

    Update 4 files

--- index.js
+++ index.js
@@ -1,73 +1,111 @@
-const express = require('express')
-const app = express()
+const express = require("express");
 const AWS = require("aws-sdk");
-const s3 = new AWS.S3()
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const app = express();
 
-app.use(bodyParser.json())
+AWS.config.update({
+  accessKeyId: process.env.ACCESS_KEY,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+const s3 = new AWS.S3();
+
+app.use(bodyParser.json());
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.BUCKET_NAME,
+    acl: "public-read", // Set the access control list to allow public read access to the uploaded files
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString()); // Use the current timestamp as the file name
+    },
+  }),
+});
 
 // curl -i https://some-app.cyclic.app/myFile.txt
-app.get('*', async (req,res) => {
-  let filename = req.path.slice(1)
+app.get("*", async (req, res) => {
+  let filename = req.path.slice(1);
 
   try {
-    let s3File = await s3.getObject({
-      Bucket: process.env.BUCKET,
-      Key: filename,
-    }).promise()
+    let s3File = await s3
+      .getObject({
+        Bucket: process.env.BUCKET,
+        Key: filename,
+      })
+      .promise();
 
-    res.set('Content-type', s3File.ContentType)
-    res.send(s3File.Body.toString()).end()
+    res.set("Content-type", s3File.ContentType);
+    res.send(s3File.Body.toString()).end();
   } catch (error) {
-    if (error.code === 'NoSuchKey') {
-      console.log(`No such key ${filename}`)
-      res.sendStatus(404).end()
+    if (error.code === "NoSuchKey") {
+      console.log(`No such key ${filename}`);
+      res.sendStatus(404).end();
     } else {
-      console.log(error)
-      res.sendStatus(500).end()
+      console.log(error);
+      res.sendStatus(500).end();
     }
   }
-})
+});
+
+app.post('/upload', upload.single('image'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
+
+  // File uploaded successfully, return the URL of the uploaded image
+  res.send(req.file.location);
+});
 
 
 // curl -i -XPUT --data '{"k1":"value 1", "k2": "value 2"}' -H 'Content-type: application/json' https://some-app.cyclic.app/myFile.txt
-app.put('*', async (req,res) => {
-  let filename = req.path.slice(1)
+app.put("*", async (req, res) => {
+  let filename = req.path.slice(1);
 
-  console.log(typeof req.body)
+  console.log(typeof req.body);
 
-  await s3.putObject({
-    Body: JSON.stringify(req.body),
-    Bucket: process.env.BUCKET,
-    Key: filename,
-  }).promise()
+  await s3
+    .putObject({
+      Body: JSON.stringify(req.body),
+      Bucket: process.env.BUCKET,
+      Key: filename,
+    })
+    .promise();
 
-  res.set('Content-type', 'text/plain')
-  res.send('ok').end()
-})
+  res.set("Content-type", "text/plain");
+  res.send("ok").end();
+});
 
 // curl -i -XDELETE https://some-app.cyclic.app/myFile.txt
-app.delete('*', async (req,res) => {
-  let filename = req.path.slice(1)
+app.delete("*", async (req, res) => {
+  let filename = req.path.slice(1);
 
-  await s3.deleteObject({
-    Bucket: process.env.BUCKET,
-    Key: filename,
-  }).promise()
+  await s3
+    .deleteObject({
+      Bucket: process.env.BUCKET,
+      Key: filename,
+    })
+    .promise();
 
-  res.set('Content-type', 'text/plain')
-  res.send('ok').end()
-})
+  res.set("Content-type", "text/plain");
+  res.send("ok").end();
+});
 
 // /////////////////////////////////////////////////////////////////////////////
 // Catch all handler for all other request.
-app.use('*', (req,res) => {
-  res.sendStatus(404).end()
-})
+app.use("*", (req, res) => {
+  res.sendStatus(404).end();
+});
 
 // /////////////////////////////////////////////////////////////////////////////
 // Start the server
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`index.js listening at http://localhost:${port}`)
-})
+  console.log(`index.js listening at http://localhost:${port}`);
+});
