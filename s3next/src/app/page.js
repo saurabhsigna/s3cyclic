25eb6cad58b4b790ddc128ad33ae4e0149ff8e78
saurabh pal <131267391+saurabhsigna@users.noreply.github.com>
2023-05-20 06:42:56 +0000

    Update page.js

--- s3next/src/app/page.js
+++ s3next/src/app/page.js
@@ -1,10 +1,11 @@
-import AWS from 'aws-sdk';
-import { useState } from 'react';
+"use client";
+import AWS from "aws-sdk";
+import { useState } from "react";
 AWS.config.update({
   accessKeyId: process.env.ACCESS_KEY,
   secretAccessKey: process.env.SECRET_ACCESS_KEY,
   region: process.env.REGION,
-  signatureVersion: 'v4',
+  signatureVersion: "v4",
 });
 
 export default function ImageUploader() {
@@ -14,31 +15,31 @@ export default function ImageUploader() {
 
   const handleFileSelect = (e) => {
     setFile(e.target.files[0]);
-  }
+  };
   const uploadToS3 = async () => {
     if (!file) {
       return;
     }
-    const params = { 
-      Bucket: process.env.BUCKET, 
-      Key: `${Date.now()}.${file.name}`, 
-      Body: file 
+    const params = {
+      Bucket: process.env.BUCKET,
+      Key: `${Date.now()}.${file.name}`,
+      Body: file,
     };
     const { Location } = await s3.upload(params).promise();
     setImageUrl(Location);
-    console.log('uploading to s3', Location);
-  }
+    console.log("uploading to s3", Location);
+  };
   return (
-    <div style={{ marginTop: '150px' }}>
+    <div style={{ marginTop: "150px" }}>
       <h1>Test Image Upload</h1>
       <input type="file" onChange={handleFileSelect} />
       {file && (
-        <div style={{ marginTop: '10px' }}>
+        <div style={{ marginTop: "10px" }}>
           <button onClick={uploadToS3}>Upload</button>
         </div>
       )}
       {imageUrl && (
-        <div style={{ marginTop: '10px' }}>
+        <div style={{ marginTop: "10px" }}>
           <img src={imageUrl} alt="uploaded" />
         </div>
       )}
